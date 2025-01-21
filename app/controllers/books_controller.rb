af518db7dff939cd6f7610737f1c5adcaf3e0fb7
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-01-21 16:21:00 +0000

    [Add & Update]add create action. updated sidebar temp.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,15 @@
 class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 end
